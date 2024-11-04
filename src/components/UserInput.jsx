dd4dd117509a5a3a989a3fc4cfc9f7ccd24908e8
Diane Cortez <c.dianekrstl@gmail.com>
2024-11-04 09:18:35 +0800

    first commit

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            };
+        });
+    }
+
+    return (
+      <section id="user-input">
+        <div className="input-group">
+          <p>
+            <label>Initial Investment</label>
+            <input
+              type="number"
+              required
+              value={userInput.initialInvestment}
+              onChange={(event) =>
+                handleChange("initialInvestment", event.target.value)
+              }
+            />
+          </p>
+          <p>
+            <label>Annual Investment</label>
+            <input type="number" required />
+          </p>
+        </div>
+        <div className="input-group">
+          <p>
+            <label>Expected Return</label>
+            <input type="number" required />
+          </p>
+          <p>
+            <label>Duration</label>
+            <input type="number" required />
+          </p>
+        </div>
+      </section>
+    );
+}
